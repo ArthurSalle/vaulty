b50310e5ce2cbc5b6bab46351a0f7a7095daf14e
Arthur Sallé <arthursalle.pro@gmail.com>
2023-10-30 16:58:47 +0100

    🥅  create error handling page

---
+++ src/components/error/error-page.tsx
@@ -0,0 +1,34 @@
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from 'react-router-dom'
+import { Button } from '../ui/button'
+
+export default function ErrorPage() {
+  const error = useRouteError()
+  const navigate = useNavigate()
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className='flex flex-col h-screen gap-4 justify-center items-center'>
+        <h1 className='text-3xl'>Oops! Something went wrong 🫠</h1>
+
+        <p className='text-lg'>
+          <i>
+            {error.status} {error.statusText && error.statusText}
+          </i>
+        </p>
+
+        <Button onClick={() => navigate(-1)}>Go back!</Button>
+      </div>
+    )
+  } else {
+    return (
+      <div className='flex flex-col h-screen gap-4 justify-center items-center'>
+        <h1 className='text-3xl'>Oops! Something went wrong 🫠</h1>
+
+        <Button onClick={() => navigate(-1)}>Go back!</Button>
+      </div>
+    )
+  }
+}
