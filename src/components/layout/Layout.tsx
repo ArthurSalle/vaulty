6d10a85eabc4a6d7f5cb976cb07b263f90d967a6
Arthur Sallé <arthur.salle95@gmail.com>
2023-11-14 11:43:28 +0100

    📈 disabled debug vercel analytics

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,15 +3,15 @@ import { NavDesktop, NavMobile } from '../../modules/shared/components/Nav'
 import { Outlet } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 
+// useMedia for render nav
 export default function Layout() {
   return (
     <div className='flex w-full h-full relative'>
       <NavDesktop />
       <NavMobile />
-
       <Outlet />
       <Toaster />
-      <Analytics />
+      <Analytics debug={false} />
     </div>
   )
 }
