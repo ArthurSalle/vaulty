39c34eb141b88334f9a65100a2906256d8c25958
Arthur Sallé <arthur.salle95@gmail.com>
2023-11-28 18:18:44 +0100

    💄 add dark mode & change color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Layout from './components/layout/Layout'
+import { ThemeProvider } from './components/themes/theme-provider'
 
 function App() {
-  return <Layout />
+  return (
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <Layout />
+    </ThemeProvider>
+  )
 }
 
 export default App
