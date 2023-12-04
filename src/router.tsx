2c8da85293946beb1ff00069f658cdbfc84b0966
Arthur Sallé <arthur.salle95@gmail.com>
2023-12-04 15:13:43 +0100

    🚚 fix settings page path in router

--- src/router.tsx
+++ src/router.tsx
@@ -43,7 +43,7 @@ import {
   CreateIdentity,
   createIdentityAction,
 } from './modules/identity/components/CreateIdentity'
-import { Settings } from './pages/settings'
+import { Settings } from './pages/Settings'
 
 export const router = createBrowserRouter([
   {
